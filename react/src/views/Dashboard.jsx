00965793926c93af10868fb59674d9e90717e264
Eduardo Lazzaretti <duadsal@gmail.com>
2023-12-18 22:25:06 -0300

    Adicionando page component

--- react/src/views/Dashboard.jsx
+++ react/src/views/Dashboard.jsx
@@ -1,7 +1,10 @@
+import PageComponent from "../components/PageComponent";
 
 export default function Dashboard() {
   return (
-    
+    <PageComponent title="DashBoard">
+      children
+    </PageComponent>
   )
 }
 
